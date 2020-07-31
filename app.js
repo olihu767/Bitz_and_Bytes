9d18fb3a47b60cdacaf33c84338894367013cea4
DaveYYC <DaveYYC@users.noreply.github.com>
2020-07-31 13:43:50 -0600

    updated Mongodb connectiion

--- app.js
+++ app.js
@@ -8,22 +8,30 @@ const moment     = require('moment');
 const today      = moment();
 const bodyParser = require('body-parser');
 
-// Our Model
-const { userInfo } = require('os');
-
 // create express app
 const app = express();
 app.set('view engine', 'ejs');
 
-// Cors origin URL - Allow inbound traffic from origin //
-// corsOptions = {
-//   origin: "https://travel-experts-prototype.herokuapp.com/",
-//   optionsSuccessStatus: 200 
-// };
-// app.use(cors(corsOptions));
-
 // automatically check if requested file is found in /public. If yes, return that file as a response to the browser
 app.use(express.static(path.join(__dirname, 'public')));
+
+const Customer = require("./models/customers.js");
+
+const mongoDB = process.env.MONGODB_URL;
+
+mongoose.connect(mongoDB, { useUnifiedTopology: true,useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors) //
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Set a callback to let us know we've successfully connected //
+db.once('open', function() {
+  console.log('Congrats! You are connected to MongoDB...');
+});
+
+app.use(bodyParser.urlencoded ({extended:true}));
 
 // Define an endpoint handlers for the home page to render 
 app.get('/', function(request, response){
@@ -42,14 +50,58 @@ app.get('/gallery', function(request, response){
   response.render('gallery',{});
 });
 
-// Define an endpoint handler for the individual destination pages
-// app.get('/:id', function(request, response){
 
+// const Agency = require("./models/agency.js");
+
+app.post ("/", function(req, res){
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const address = req.body.address;
+  const city = req.body.city;
+  const prov = req.body.prov;
+  const postal = req.body.postal;
+  const country = req.body.country;
+  const homePhone = req.body.homePhone;
+  const busPhone = req.body.busPhone;
+  const userid = req.body.userid;
+  const pswwd = req.body.psswd;
+  const email = req.body.email;
+  
+  const newCustomer = {
+    CustFirstName: firstName,
+    CustLastName: lastName,
+    CustAddress: address,
+    CustCity: city,
+    CustProv: prov,
+    CustPostal: postal,
+    CustCountry: country,
+    CustHomePhone: homePhone,
+    CustBusPhone: busPhone,
+    CustUserId: userid,
+    CustPsswd: psswd,
+    CustEmail: email,
+  
+};
+// create a new customer into DB
+Customer.create(newCustomer, function(err, newlyCreated){
+  if (err){
+    console.log(err)
+  } else {
+    console.log(newCustomer)
+    res.redirect("/")
+  }
+})
+});
+  
+// app.get("/order/package", function(req,res){
+//   res.render("order.ejs")
+// });
 
 // add current year using moment module
 const yearFormat="YYYY"
-app.locals.moment = moment;
-app.locals.yearFormat = yearFormat;
+  app.locals.moment = moment;
+  app.locals.yearFormat = yearFormat;
 
 // start up server
 const PORT = process.env.PORT || 3000;
@@ -59,30 +111,4 @@ app.listen(PORT, function(){
 });
 
 
-mongoose.connect("mongodb://localhost:27017/register_user", { useUnifiedTopology: true,useNewUrlParser: true });
-
-//SCHEMA SETUP
-const register_userSchema = new mongoose.Schema({
-  name: String,
-});
-
-const Register_user = mongoose.model("register_user", register_userSchema);
-
-Register_user.create(
-  {
-    name: "name_input"
-  }, function(err, register_user){
-    if(err){
-      console.log(err);
-
-      }
-       else {
-         console.log("new registered user: ");
-         console.log(register_usser);
-       }
-    });
-    
-const register_user = [
-  {name: "Dave"}
-]
 
---
+++ public/js/customers.js
@@ -0,0 +1,21 @@
+const mongoose   = require('mongoose');
+
+const CustomerSchema = new mongoose.Schema(
+  {
+    // CustomerId: Number,
+    CustFirstName: String,
+    CustLastName: String,
+    CustAddress: String,
+    CustCity: String,
+    CustProv: String,
+    CustPostal: String,
+    CustCountry: String,
+    CustHomePhone: String,
+    CustBusPhone: String,
+    CustEmail: String,
+    CustUserid: String,
+    CustPsswd: String
+  }
+);
+
+module.exports = mongoose.model('Customer', CustomerSchema);
