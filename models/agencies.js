c7496947d060dd34c5ecf216bbb1dd0e2862a26f
DaveYYC <DaveYYC@users.noreply.github.com>
2020-08-06 18:55:39 -0600

    updated pages

--- models/agencies.js
+++ models/agencies.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const AgenciesSchema = new mongoose.Schema(
   {
-    AgencyId:      Number,
-    AgncyAddress:  String,
-    AgncyCity:     String,
-    AgncyProv:     String,
-    AgncyPostal:   String,
-    AgncyCountry:  String,
-    AgncyPhone:    String,
-    AgncyFax:      String,
+    AgencyId:     Number,
+    AgncyAddress: String,
+    AgncyCity:    String,
+    AgncyProv:    String,
+    AgncyPostal:  String,
+    AgncyCountry: String,
+    AgncyPhone:   String,
+    AgncyFax:     String,
   }
 );
 
--- public/js/packages.js
+++ public/js/packages.js
@@ -1,34 +1,33 @@
 // Fetch the data from the packages database in MongoDB
 
-fetch('http://localhost:3000/api/packages') 
-// fetch('https:// https://travel-experts-prototype.herokuapp.com/api/packages')
-  .then(function(response) {
+fetch('http://localhost:3000/api/packages')
+  // fetch('https:// https://travel-experts-prototype.herokuapp.com/api/packages')
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
+  .then(function (data) {
 
     const packagesList = data;
 
-    let packagesTemplate = ''; 
+    let packagesTemplate = '';
 
-    const packages = document.querySelector('.packages'); 
+    const packages = document.querySelector('.packages');
 
-    packagesList.forEach(function(item){
-      packagesTemplate += 
+    packagesList.forEach(function (item) {
+      packagesTemplate +=
         `<figure>
-          ${item.PkgName}
-          <img src="https://picsum.photos/id/${item.ImgId}/400" alt="${item.PkgName}">
+          <label>${item.PkgName}</label>
+          <img src="https://picsum.photos/id/${item.ImgId}/375" alt="${item.PkgName}">
           </a>
           <ul>
             <li>${item.PkgDesc}</li><br>
             <li>Promotion Start Date: ${item.PkgStartDate}</li>
             <li>Promotion End Date: ${item.PkgEndDate}</li><br>
             <li>Book today starting from only: $${item.PkgBasePrice} CAD</li>
-            <button><a href = "/${item.PkgPage}">Book Now!</a></button>
+            <button><a href ="/${item.PkgPage}">Book Now!</a></button>
           </ul>
         </figure>`;
-   
-    packages.innerHTML = packagesTemplate;  
+
+      packages.innerHTML = packagesTemplate;
     });
-});
-    
+  });
