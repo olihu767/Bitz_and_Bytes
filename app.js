0bfd4ec3334464097b476059f718805be63f1cec
DaveYYC <DaveYYC@users.noreply.github.com>
2020-08-09 18:08:37 -0600

    tweak for Heroku

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ db.once('open', function() {
 
 // Cors origin URL - Allow inbound traffic from origin //
 corsOptions = {
-  origin: "https://bitsandbytes.herokuapp.com",
+  origin: "https://travel-experts-prototype.herokuapp.com",
   optionsSuccessStatus: 200 
   };
   app.use(cors(corsOptions));
